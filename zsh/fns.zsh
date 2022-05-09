8bcad12c59b2a0ec7ae630fd2a0aced90579fbe6
edwmurph <edward@edwardmurphy.dev>
2022-05-09 16:13:55 -0400

    fix tt

--- zsh/fns.zsh
+++ zsh/fns.zsh
@@ -45,10 +45,8 @@ function tt() {
   local ignore_dirs="$(pipe_deliminate $IGNORE_DIRS)|$(pipe_deliminate $gitignore)"
   local ignore_dirs_escaped="$(echo $ignore_dirs | sed 's/*/\\*/g')"
 
-  tree -a -L 10 -C -f -n -F --noreport "${1-.}" \
-    | grep -v -E "$ignore_dirs_escaped" \
-    | sed 's/\/$//;s/\..*\///'
-    # | sed 's/\/$//;s/\/.*\///;s/\..*\///'
+  tree -a -L 10 -C -n -F --noreport "${1-.}" \
+    | grep -v -E "$ignore_dirs_escaped"
 }
 
 function nrd() {
