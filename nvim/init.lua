2e883ada12cba0793e8e0fcb54e2cb6101536423
edwmurph <20996513+edwmurph@users.noreply.github.com>
2026-05-04 11:42:20 -0400

    tweaks

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,6 +5,7 @@ vim.g.mapleader = ' '
 vim.wo.wrap = false
 opt.mouse = 'a'
 opt.number = true
+opt.clipboard = 'unnamedplus'
 vim.g.columns=80
 vim.cmd([[set colorcolumn=80]])
 vim.cmd([[set title]])
@@ -30,7 +31,9 @@ vim.keymap.set('n', '<Leader>;', '<Esc> :w <Enter>')
 vim.keymap.set('n', '<c-@>', ':FZF <Enter>')
 
 -- visual mode mappings
-vim.keymap.set('v', '<c-c>', '"*y :let @+=@*<CR>')
+vim.keymap.set('v', '<C-c>', '"+y', { desc = 'Copy selection to clipboard' })
+vim.keymap.set('i', '<C-v>', '<C-r>+', { desc = 'Paste from clipboard' })
+vim.keymap.set({ 'n', 'v' }, '<C-v>', '"+p', { desc = 'Paste from clipboard' })
 
 require('codex_pane').setup()
 require 'plugs'
